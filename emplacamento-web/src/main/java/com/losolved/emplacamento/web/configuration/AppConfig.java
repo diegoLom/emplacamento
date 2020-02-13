ef3a6eea5864d10e38e52006c47d8bf68f1c239f
diegosantosAdn <diegosantana019@gmail.com>
2020-02-13 11:26:42 -0300

    heroku configurations

--- emplacamento-web/src/main/java/com/losolved/emplacamento/web/configuration/AppConfig.java
+++ emplacamento-web/src/main/java/com/losolved/emplacamento/web/configuration/AppConfig.java
@@ -1,9 +1,16 @@
 package com.losolved.emplacamento.web.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @EntityScan(value = "com.losolved.emplacamento.domain")
@@ -12,8 +19,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 public class AppConfig {
 	
+	@Value("${spring.datasource.url}")
+	private String dbUrl; 
 	
 	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig(); 
+		config.setJdbcUrl(dbUrl);
+		
+		return new HikariDataSource(config);
+	}
 
 
 }
