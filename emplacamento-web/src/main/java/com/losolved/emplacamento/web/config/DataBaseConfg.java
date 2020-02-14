9a63bc9f83f369bb83b33e59b87fdc15b7e44c57
diegoLom <diegosantana019@gmail.com>
2020-02-13 22:11:28 -0300

    adding cofing

---
+++ emplacamento-web/src/main/java/com/losolved/emplacamento/web/config/DataBaseConfg.java
@@ -0,0 +1,27 @@
+package com.losolved.emplacamento.web.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DataBaseConfg {
+	
+	
+
+	  @Value("${spring.datasource.url}")
+	  private String dbUrl;
+
+	  @Bean
+	  public DataSource dataSource() {
+	      HikariConfig config = new HikariConfig();
+	      config.setJdbcUrl(dbUrl);
+	      return new HikariDataSource(config);
+	  }
+
+}
