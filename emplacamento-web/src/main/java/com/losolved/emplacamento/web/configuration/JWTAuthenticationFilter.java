7e32697876cbb26ede2b0b680a44c0699da0113b
diegoLom <diegosantana019@gmail.com>
2020-03-05 17:26:26 -0300

    adicionando filtro

---
+++ emplacamento-web/src/main/java/com/losolved/emplacamento/web/configuration/JWTAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.losolved.emplacamento.web.configuration;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JWTAuthenticationFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		
+		Authentication authentication = TokenAuthenticationService
+				.getAuthentication((HttpServletRequest) request);
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		filterChain.doFilter(request, response);
+	}
+
+
+}
