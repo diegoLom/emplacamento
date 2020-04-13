2e737ce3d11eea1a151b67c9c899ff69bf559c70
diegoLom <diegosantana019@gmail.com>
2020-04-13 15:23:11 -0300

    prj

---
+++ emplacamento-integration/src/main/java/com/losolved/emplacamento/integration/sdk/UsuarioResult.java
@@ -0,0 +1,58 @@
+package com.losolved.emplacamento.integration.sdk;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UsuarioResult {
+	
+	
+	private String codigo_funcionario;
+	private String nome;
+	private String login_funcionario;
+	private String tipo;
+	private String supervisor;
+	public String getCodigo_funcionario() {
+		return codigo_funcionario;
+	}
+	public void setCodigo_funcionario(String codigo_funcionario) {
+		this.codigo_funcionario = codigo_funcionario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin_funcionario() {
+		return login_funcionario;
+	}
+	public void setLogin_funcionario(String login_funcionario) {
+		this.login_funcionario = login_funcionario;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getSupervisor() {
+		return supervisor;
+	}
+	public void setSupervisor(String supervisor) {
+		this.supervisor = supervisor;
+	}
+	
+	@Override
+	public String toString() {
+		return "UsuarioResult [codigo_funcionario=" + codigo_funcionario + ", nome=" + nome + ", login_funcionario="
+				+ login_funcionario + ", tipo=" + tipo + ", supervisor=" + supervisor + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
